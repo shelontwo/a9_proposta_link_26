fdbd480527327850bdee8f373bc0af34e031d6ac
Mauricio kruger <mau_kriger@hotmail.com>
2026-02-05 12:56:30 -0300

    refactor(start): atualizado

--- start.sh
+++ start.sh
@@ -1,18 +1,22 @@
 #!/bin/bash
 
-echo "Iniciando sistema..."
+echo "Limpando portas antigas..."
+# O comando fuser mata qualquer processo travado nas portas
+fuser -k 3000/tcp
+fuser -k 3001/tcp
 
 # Backend
+echo "Iniciando Backend na porta 3001..."
 cd /app/backend
-echo "Iniciando Backend na porta $PORT..."
-node src/app.js & 
+# Forçamos a porta 3001 apenas para este comando
+PORT=3001 node src/app.js & 
 
-sleep 3
+sleep 5
 
 # Frontend
+echo "Iniciando Frontend na porta 3000..."
 cd /app/frontend
-echo "Iniciando Frontend..."
-# Usamos HOSTNAME=0.0.0.0 para permitir que o Easypanel acesse o container
-HOSTNAME=0.0.0.0 npm run dev &
+# Forçamos a porta 3000 e o host para o Next.js
+PORT=3000 HOSTNAME=0.0.0.0 npm run dev &
 
 wait -n
