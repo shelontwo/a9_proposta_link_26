ffd7947ba5a3ce9c32a48f383cdcb568e87413e5
Mauricio kruger <mau_kriger@hotmail.com>
2026-02-05 11:26:06 -0300

    refactor(changes): ajustado configuracoes

--- start.sh
+++ start.sh
@@ -3,12 +3,9 @@
 echo "Instalando/Iniciando processos..."
 
 # Backend
-cd /app/backend
-npm start &
+cd /app/backend && npm start &
 
 # Frontend
-cd /app/frontend
-# O --host é vital para o Easypanel conseguir acessar a porta
-npm run dev -- --host 0.0.0.0 &
+cd /app/frontend && npm run dev -- --host 0.0.0.0 &
 
 wait -n
