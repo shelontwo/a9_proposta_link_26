60190164fb9469c876d89471a140a72b4cfe47ee
Mauricio kruger <mau_kriger@hotmail.com>
2026-02-05 11:49:48 -0300

    refactor(configs): atualizadas

--- start.sh
+++ start.sh
@@ -1,11 +1,14 @@
 #!/bin/bash
 
-echo "Instalando/Iniciando processos..."
-
 # Backend
-cd /app/backend && npm start &
+echo "Iniciando Backend..."
+cd /app/backend && node src/app.js & 
+
+# Espera um pouco
+sleep 2
 
 # Frontend
+echo "Iniciando Frontend..."
 cd /app/frontend && npm run dev -- --host 0.0.0.0 &
 
 wait -n
