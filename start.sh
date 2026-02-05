306231f371082d73e22c55a02bf73662795562db
Mauricio kruger <mau_kriger@hotmail.com>
2026-02-05 13:17:22 -0300

    refactor(files): alterado para build

--- start.sh
+++ start.sh
@@ -1,22 +1,18 @@
 #!/bin/bash
 
-echo "Limpando portas antigas..."
-# O comando fuser mata qualquer processo travado nas portas
-fuser -k 3000/tcp
-fuser -k 3001/tcp
+echo "Iniciando sistema em modo PRODUÇÃO..."
 
 # Backend
-echo "Iniciando Backend na porta 3001..."
 cd /app/backend
-# Forçamos a porta 3001 apenas para este comando
-PORT=3001 node src/app.js & 
+echo "Iniciando Backend na porta 3001..."
+node src/app.js & 
 
 sleep 5
 
 # Frontend
-echo "Iniciando Frontend na porta 3000..."
 cd /app/frontend
-# Forçamos a porta 3000 e o host para o Next.js
-PORT=3000 HOSTNAME=0.0.0.0 npm run dev &
+echo "Iniciando Frontend na porta 3000..."
+# Usamos o 'npm start' em vez do 'npm run dev'
+PORT=3000 HOSTNAME=0.0.0.0 npm start &
 
 wait -n
